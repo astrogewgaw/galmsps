9dad0cf7725e36c8c58e8c180d8d46d98b9742a4
astrogewgaw <ujjwalpanda97@gmail.com>
2021-05-21 17:45:20 +0530

    :recycle: Refactoring how scraping works.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Code style: black][black-badge]][black]
 
-this is my own copy of the [**galactic MSPs database**][webcat], which is maintained as a simple plain text file [**here**][txtcat]. the code to scrap the database is in the [**scrap.py**](scrap.py) file, and the database itself (in all its JSONic glory) is in the [**galmsps.json**](galmsps.json) file. the data is updated on the 14th of every month, at midnight. this repository will eventually power the [**koshka**][koshka] package, which aims to make accessing all pulsar and radio transient related catalogues easier.
+this is my own copy of the [**galactic MSPs database**][webcat], which is maintained as a simple plain text file [**here**][txtcat]. the code to scrap the database is in the [**scrap.py**](scrap.py) file, and the database itself (in all its JSONic glory) is in the [**galmsps.json**](galmsps.json) file. the data is updated on every Friday, at midnight (in UTC time). this repository will eventually power the [**koshka**][koshka] package, which aims to make accessing all pulsar and radio transient related catalogues easier.
 
 [black]: https://github.com/psf/black
 [koshka]: https://github.com/astrogewgaw/koshka
--- scrap.py
+++ scrap.py
@@ -1,89 +1,54 @@
-import re
-import json
-import requests
+if __name__ == "__main__":
 
-from schema import Use, Schema  # type: ignore
+    import re
 
+    from json import dump
+    from requests import get
 
-# URL of the galactic MSPs catalog.
-url = "http://astro.phys.wvu.edu/GalacticMSPs/GalacticMSPs.txt"
+    numeric = (
+        lambda _: float(_)
+        if re.search(re.compile(r"^[-+]?[0-9]*[.]?[0-9]+([eE][-+]?[0-9]+)?$"), _)
+        else None
+    )
 
-# A regular expression to match comments.
-# This is used to remove the comments at
-# the beginning of the database.
-comment = re.compile(
-    r"""
-    ^       # Beginning of line.
-    ([^#]*) # Do not match zero or more hash characters.
-            # This takes care of not matching lines that
-            # start with multiple hash characters.
-    [#]     # The hash character.
-    (.*)    # Match zero or more instances of any character.
-    $       # End of line.
-    """,
-    re.VERBOSE | re.MULTILINE,
-)
-
-
-# A regular expression to match any floating point number,
-# such as "9", "9.0", "+8.67" or "-0.67". The number can
-# also be in scientific notation ("1e-9").
-fpnex = re.compile(
-    r"""
-    ^                   # Beginning of string.
-    [-+]?               # Match zero or more instances of a plus/minus sign.
-    [0-9]*              # Match zero or more instances of a numeric character.
-    [.]?                # Match zero or more instances of a decimal point.
-    [0-9]+              # Match one or more instances of a numeric character.
-    ([eE][-+]?[0-9]+)?  # Match an exponent, as in "1e-9".
-                        # This pattern matches a sign too,
-                        # if present.
-    $                   # End of string.
-    """,
-    re.VERBOSE,
-)
-
-# A simple function to deal with numeric values that could possibly be NULL.
-isnull = lambda x: float(x) if re.search(fpnex, x) else None
-
-# The key-value map for the database.
-kvs = {
-    "NAME": Use(str),
-    "P": Use(isnull),
-    "DM": Use(isnull),
-    "GAL_L": Use(isnull),
-    "GAL_B": Use(isnull),
-    "PBIN": Use(isnull),
-    "PSMA": Use(isnull),
-    "YEAR": Use(int),
-    "NOTES": Use(str),
-}
-
-
-def scrap() -> None:
-
-    """
-    Scrap the galactic MSPs database and serialise it to a JSON file.
-    """
-
-    cat = requests.get(url)
-    text = cat.content.decode()
-    data = re.sub(comment, "", text).strip()
-    rows = [[_ for _ in re.split(r"\s+", _) if _] for _ in re.split(r"\n+", data)]
-    mspp = lambda keys, vals: {key: val for key, val in zip(keys, vals)}
-    galmsps = {
-        str(i + 1): Schema(kvs).validate(mspp(kvs.keys(), row))
-        for i, row in enumerate(rows)
+    data = {
+        str(i + 1): {
+            key: conv(value)  # type: ignore
+            for (key, conv), value in zip(
+                [
+                    ("NAME", str),
+                    ("P", numeric),
+                    ("DM", numeric),
+                    ("GAL_L", numeric),
+                    ("GAL_B", numeric),
+                    ("PBIN", numeric),
+                    ("PSMA", numeric),
+                    ("YEAR", int),
+                    ("NOTES", str),
+                ],
+                values,
+            )
+        }
+        for i, values in enumerate(
+            [
+                [_ for _ in re.split(r"\s+", _) if _]
+                for _ in re.split(
+                    r"\n+",
+                    re.sub(
+                        re.compile(r"^([^#]*)[#](.*)$", re.MULTILINE),
+                        "",
+                        get(
+                            "http://astro.phys.wvu.edu/GalacticMSPs/GalacticMSPs.txt"
+                        ).content.decode(),
+                    ).strip(),
+                )
+            ]
+        )
     }
 
     with open("galmsps.json", "w+") as fobj:
-        json.dump(
-            galmsps,
-            fobj,
+        dump(
+            obj=dict(data=data),
+            fp=fobj,
             indent=4,
         )
-
-
-if __name__ == "__main__":
-
-    scrap()
